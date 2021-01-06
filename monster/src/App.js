cdf7fef78efd3803cfb64d4d7fc5660196a928e3
JM <manzur-1990@hotmail.com>
2021-01-05 18:18:53 -0600

    card component

--- monster/src/App.js
+++ monster/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
+
+//Components
 import { CardList } from './components/card-list/card-list.components'
-
-
-
 export default class App extends Component {
   state = {
     monsters: []
-  }
+  };
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
-  }
+  };
 
   render() {
     return (
@@ -20,5 +19,5 @@ export default class App extends Component {
       <CardList monsters={this.state.monsters} /> 
       </div>
     )
-  }
-}
+  };
+};
